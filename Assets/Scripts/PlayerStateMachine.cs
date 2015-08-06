46f65019e5ce253873943c29ac6e8a8379aefb4c
keonen <keijo.olavi.kangas@gmail.com>
2015-08-06 10:44:19 +0300

    2015-08-06 10:44

--- Assets/Scripts/PlayerStateMachine.cs
+++ Assets/Scripts/PlayerStateMachine.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
-
+public class PlayerStateMachine : MonoBehaviour {
+	
 	public float Speed = 3f;
 	public float MaxJumpTime = 2f;
 	private float move = 0f;
@@ -12,22 +12,24 @@ public class PlayerMovement : MonoBehaviour {
 	private bool CanJump;
 	private bool playerOnTheGround;
 	public Animator anim;
-
+	enum CharacterState {OnGround, InAir, Climbing};
+	CharacterState characterState = CharacterState.OnGround;
+	
 	void OnCollisionStay2D (Collision2D col)
 	{
 		if(col.gameObject.tag == "Ground")
 		{
 			playerOnTheGround = true;
-			//Debug.Log ("Pelaaja osuu maahan!");
+			Debug.Log ("Pelaaja osuu maahan!");
 		}
 	}
-
+	
 	void OnCollisionExit2D (Collision2D col)
 	{
 		if(col.gameObject.tag == "Ground")
 		{
 			playerOnTheGround = false;
-			//Debug.Log ("Pelaaja ei osu maahan!");
+			Debug.Log ("Pelaaja ei osu maahan!");
 		}
 	}
 	
@@ -35,12 +37,12 @@ public class PlayerMovement : MonoBehaviour {
 	{
 		// Get the Animator component from your gameObject
 		anim = GetComponent<Animator>();
-
+		
 	}
 	
 	void Update ()
 	{
-
+		
 		if (!CanJump)
 			JumpTime  -= Time.deltaTime;
 		if (JumpTime <= 0)
@@ -48,49 +50,62 @@ public class PlayerMovement : MonoBehaviour {
 			CanJump = true;
 			JumpTime  = MaxJumpTime;
 		}
-
+		
 		if(Input.GetKeyDown(KeyCode.R))
 		{
 			Application.LoadLevel(Application.loadedLevel);
 		}
-
+		
 		playerVelocity = Mathf.Abs(GetComponent<Rigidbody2D> ().velocity.x);
-
+		
 		// Sets the value
 		anim.SetFloat ("velocity", playerVelocity);
-
-
+		
+		switch (characterState)
+		{
+		case CharacterState.OnGround:
+			float horizontal = Input.GetAxis("Horizontal");
+			
+			anim.SetFloat("velocity", Mathf.Abs(horizontal));
+			break;
+		case CharacterState.InAir:
+			break;
+		case CharacterState.Climbing:
+			break;
+			
+		}
+		
 	}
-
+	
 	void FixedUpdate () {
 		move = Input.GetAxis ("Horizontal");
 		GetComponent<Rigidbody2D>().velocity = new Vector2 (move * Speed, GetComponent<Rigidbody2D>().velocity.y);
-
+		
 		if (((Input.GetKeyDown (KeyCode.Space)) || (Input.GetKeyDown (KeyCode.W))) && CanJump && playerOnTheGround)
 		{
 			GetComponent<Rigidbody2D> ().AddForce (new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, JumpForce));
-
+			
 			CanJump = false;
 			JumpTime = MaxJumpTime;
 		}
-
+		
 		float checkDirection = Mathf.Sign(GetComponent<Rigidbody2D> ().velocity.x);
-
+		
 		if (Input.GetKey (KeyCode.A) && checkDirection == -1)
 		{
 			transform.localRotation = Quaternion.Euler(0, 180, 0);
-		
+			
 			//transform.Rotate (Vector3.up * -180 * checkDirection);
-			//Debug.Log(checkDirection);
+			Debug.Log(checkDirection);
 		}
-
+		
 		if (Input.GetKey (KeyCode.D) && checkDirection == 1)
 		{
 			transform.localRotation = Quaternion.Euler(0, 0, 0);
-
+			
 			//transform.Rotate (Vector3.up * -180 * checkDirection);
-			//Debug.Log(checkDirection);
+			Debug.Log(checkDirection);
 		}
-	
+		
 	}
 }
